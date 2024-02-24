330a8bbf2e5ba2388f6684d029885b11ba1ff054
Jeenyhus <mweembadabwitso@gmail.com>
2024-02-24 18:10:06 +0200

    changed urls

--- timesheet/urls.py
+++ timesheet/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('time-sheet/', views.time_sheet, name='time_sheet'),
     path('invoice/', views.invoice, name='invoice'),
-    path('generate-invoice/<int:time_sheet_entry_id>/', views.generate_invoice, name='generate_invoice'),
     path('invoice/<int:invoice_id>/', views.invoice_detail, name='invoice_detail'),
+    path('create-task/', views.create_task, name='create_task'),
 ]
--- timesheet/views.py
+++ timesheet/views.py
@@ -18,42 +18,15 @@ def time_sheet(request):
     return render(request, 'time_sheet.html', {'form': form, 'time_sheet_entries': time_sheet_entries})
 
 def invoice(request):
-    # Retrieve all invoices from the database
+    if request.method == 'POST':
+        form = InvoiceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('invoice')
+    else:
+        form = InvoiceForm()
     invoices = Invoice.objects.all()
-    # Render the template with invoices
-    return render(request, 'invoice.html', {'invoices': invoices})
-
-def generate_invoice(request, time_sheet_entry_id):
-    # Retrieve the time sheet entry based on the provided ID
-    time_sheet_entry = get_object_or_404(TimeSheetEntry, pk=time_sheet_entry_id)
-    
-    # Perform operations to generate an invoice based on the time sheet entry
-    total_hours_worked = time_sheet_entry.duration.total_seconds() / 3600  # Convert duration to hours
-    rate_per_hour = time_sheet_entry.franchisee.hourly_rate
-    amount_payable = total_hours_worked * rate_per_hour
-
-    # Create an Invoice object
-    invoice = Invoice(
-        franchisee=time_sheet_entry.franchisee,
-        invoice_number='',  # You may generate or assign an invoice number here
-        invoice_date=time_sheet_entry.date,
-        description_of_services=time_sheet_entry.task.description,
-        hours_worked=time_sheet_entry.duration,
-        rate=rate_per_hour,
-        other_expenses=0,  # Update if there are other expenses
-        amount_payable=amount_payable,
-        payment_instructions='Please pay within 30 days.',  # Example payment instructions
-    )
-
-    # Save the invoice to the database
-    invoice.save()
-
-    # Optionally, you can update the time sheet entry to mark it as invoiced
-    time_sheet_entry.invoiced = True
-    time_sheet_entry.save()
-
-    # Redirect to the invoice detail page
-    return redirect('invoice_detail', invoice_id=invoice.pk)
+    return render(request, 'invoice.html', {'form': form, 'invoices': invoices})
 
 def invoice_detail(request, invoice_id):
     # Retrieve the invoice based on the provided ID
