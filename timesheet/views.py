611434676fe14fd3d8a21864b876e04e77ea1d30
Jeenyhus <mweembadabwitso@gmail.com>
2024-02-24 08:51:40 +0200

    added api end points

--- timesheet/views.py
+++ timesheet/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Career, Franchisor, Task, Franchisee, Invoice
+from .serializers import CareerSerializer, FranchisorSerializer, TaskSerializer, FranchiseeSerializer, InvoiceSerializer
 
-# Create your views here.
+class CareerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows careers to be viewed or edited.
+    """
+    queryset = Career.objects.all()
+    serializer_class = CareerSerializer
+
+class FranchisorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows franchisors to be viewed or edited.
+    """
+    queryset = Franchisor.objects.all()
+    serializer_class = FranchisorSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tasks to be viewed or edited.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class FranchiseeViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows franchisees to be viewed or edited.
+    """
+    queryset = Franchisee.objects.all()
+    serializer_class = FranchiseeSerializer
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows invoices to be viewed or edited.
+    """
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
